2a14c693063366e2003230907fd3bc7c44b96254
Leslie <764608473@qq.com>
2019-10-21 10:12:04 +0800

    图标字体

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,16 +8,16 @@ class Footer extends React.Component{
     return (
       <div className="footer-container">
         <div>
-          <Link to="/">首页</Link>
+          <Link className="iconfont icon-xuanxiang" to="/">首页</Link>
         </div>
         <div>
-          <Link to="/classify">分类</Link>
+          <Link className="iconfont icon-Search" to="/classify">分类</Link>
         </div>
         <div>
-          <Link to="/shopcart">购物车</Link>
+          <Link className="iconfont icon-shopping_cart" to="/shopcart">购物车</Link>
         </div>
         <div>
-          <Link to="/profile">个人</Link>
+          <Link className="iconfont icon-geren" to="/profile">个人</Link>
         </div>
       </div>
     )
